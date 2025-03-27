a12c24467d4c4b6c5404db00654918a0783604e2
Igor Mendes Gomes <igormendesgomesin1@outlook.com>
2025-03-27 20:22:00 -0300

    Api Finished

--- MinimalApi/MinimalApi/Program.cs
+++ MinimalApi/MinimalApi/Program.cs
@@ -3,7 +3,7 @@ using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configuração do banco de dados MySQL
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySQL(connectionString));
@@ -12,9 +12,6 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-
-
-// Mapear os endpoints
 app.MapNotaEndpoints();
 
 app.Run();
--- MinimalApi/MinimalApi/obj/Debug/net8.0/MinimalApi.AssemblyInfo.cs
+++ MinimalApi/MinimalApi/obj/Debug/net8.0/MinimalApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4a94d2179c295ae7f0e936e41d7b8550dd4bb62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3c988e9c825d0a75fb42321d073bf309e20d5b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
